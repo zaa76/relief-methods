464f722ccc25c403ecb85a08350e4d3cfbc91443
zaa76 <zimasanya@gmail.com>
2022-07-20 16:58:41 +0300

    Update smoothing_tools.sh

--- smoothing_tools.sh
+++ smoothing_tools.sh
@@ -1,3 +1,4 @@
+# Pathing GDAL_CONTOUR tool for better smoothing contour lines
 wget https://trac.osgeo.org/gdal/raw-attachment/ticket/5848/0001-Contours-smoothing-by-sliding-averaging-algorithm.patch
 wget http://download.osgeo.org/gdal/2.0.2/gdal-2.0.2.tar.gz
 tar -xzvf gdal-2.0.2.tar.gz
@@ -14,16 +15,18 @@ make -j 6
 cp apps/gdal_contour /opt/relief_build/tools/gdal_contour_smooth
 popd
 
-
+# Rust compiler for build WHITEBOX_TOOLS
 curl https://sh.rustup.rs -sSf | sh
 ln -s ~/.cargo/bin/cargo -t /usr/local/bin/
 
- wget https://github.com/jblindsay/whitebox-tools/archive/refs/tags/v2.0.0.tar.gz
- tar -xzvf v2.0.0.tar.gz
- cd whitebox-tools-2.0.0 
- cargo build --release
- cp target/release whitebox_tools /opt/relief_build/tools
- 
+# whiebox_tools
+wget https://github.com/jblindsay/whitebox-tools/archive/refs/tags/v2.0.0.tar.gz
+tar -xzvf v2.0.0.tar.gz
+cd whitebox-tools-2.0.0 
+cargo build --release
+cp target/release whitebox_tools /opt/relief_build/tools
+
+# mdenoise
 wget http://www.cs.cf.ac.uk/meshfiltering/index_files/Doc/mdsource.zip
 unzip mdsource.zip
 cd mdenoise
