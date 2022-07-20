7aae1493e62a0914a87b2a4f546c0748101f9c12
zaa76 <zimasanya@gmail.com>
2022-07-20 16:55:28 +0300

    Update dependencies_install _Debian10.sh

--- dependencies_install _Debian10.sh
+++ dependencies_install _Debian10.sh
@@ -184,22 +184,7 @@ CXXFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1" ./configure \
 make -j 4 && make install && ldconfig
 popd
 
-# gdal_contour with smoothing patch
-wget https://trac.osgeo.org/gdal/raw-attachment/ticket/5848/0001-Contours-smoothing-by-sliding-averaging-algorithm.patch
-wget http://download.osgeo.org/gdal/2.0.2/gdal-2.0.2.tar.gz
-tar -xzvf gdal-2.0.2.tar.gz
-pushd gdal-2.0.2
-patch -p2 -i ../0001-Contours-smoothing-by-sliding-averaging-algorithm.patch
-./autogen.sh
-CXXFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1" ./configure \
-	--enable-shared=no \
-	--enable-static=yes \
-	--without-ld-shared \
-	--prefix=/usr/local \
-	--with-libjson-c=internal
-make -j 6	
-cp apps/gdal_contour /usr/local/bin/gdal_contour_smooth
-popd
+
 
 wget https://github.com/postgis/postgis/archive/refs/tags/3.2.1.tar.gz -O postgis.tar.gz
 tar -xzvf postgis.tar.gz
@@ -241,21 +226,3 @@ cd ImageMagick*
 	--with-openjp2=yes \
 	--with-gslib=yes
 make -j 4 && make install && ldconfig && cd ..
-
-curl https://sh.rustup.rs -sSf | sh
-ln -s ~/.cargo/bin/cargo -t /usr/local/bin/
-
- wget https://github.com/jblindsay/whitebox-tools/archive/refs/tags/v2.0.0.tar.gz
- tar -xzvf v2.0.0.tar.gz
- cd whitebox-tools-2.0.0 
- cargo build --release
- cp target/release whitebox_tools /opt/relief_build/tools
- 
-wget http://www.cs.cf.ac.uk/meshfiltering/index_files/Doc/mdsource.zip
-unzip mdsource.zip
-cd mdenoise
-g++ -Wno-write-strings -o mdenoise mdenoise.cpp triangle.c
-cp mdenoise /opt/relief_build/tools
- 
-
-
