b909a4cccab2360043fc807fd055cd461044c7a2
zaa76 <zimasanya@gmail.com>
2022-07-30 16:03:33 +0300

    Update smoothing_tools.sh

--- docs/smoothing_tools.sh
+++ docs/smoothing_tools.sh
@@ -12,7 +12,7 @@ CXXFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1" ./configure \
 	--prefix=/usr/local \
 	--with-libjson-c=internal
 make -j 6	
-cp apps/gdal_contour /opt/relief_build/tools/gdal_contour_smooth
+cp apps/gdal_contour <root build relief>/tools/gdal_contour_smooth
 popd
 
 # Rust compiler for build WHITEBOX_TOOLS
@@ -25,11 +25,11 @@ wget https://github.com/jblindsay/whitebox-tools/archive/refs/tags/v2.0.0.tar.gz
 tar -xzvf v2.0.0.tar.gz
 cd whitebox-tools-2.0.0 
 cargo build --release
-cp target/release whitebox_tools /opt/relief_build/tools
+cp target/release whitebox_tools <root build relief>/tools
 
 # mdenoise
 wget http://www.cs.cf.ac.uk/meshfiltering/index_files/Doc/mdsource.zip
 unzip mdsource.zip
 cd mdenoise
 g++ -Wno-write-strings -o mdenoise mdenoise.cpp triangle.c
-cp mdenoise /opt/relief_build/tools
+cp mdenoise <root build relief>/tools
