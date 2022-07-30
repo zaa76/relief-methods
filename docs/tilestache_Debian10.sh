7b12d61f20890575c47c81f2c501939099fe6148
zaa76 <zimasanya@gmail.com>
2022-07-30 16:51:39 +0300

    Update tilestache_Debian10.sh

--- docs/tilestache_Debian10.sh
+++ docs/tilestache_Debian10.sh
@@ -1,4 +1,4 @@
 cd /opt
-virtualenv --python=/usr/bin/python3 --prompt="(tilestache ENV)" tilestache
+virtualenv --python=/usr/bin/python2 --prompt="(tilestache ENV)" tilestache
 sed -i 's/PS1="(tilestache ENV)\${PS1-}"/PS1="\\\[\\033\[1;33m\\\](tilestache ENV)\${PS1-}"/' tilestache/bin/activate
 source tilestache/bin/activate	
