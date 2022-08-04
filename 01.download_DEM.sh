45be312457305c5ad61092c697e4208a8f06423d
zaa76 <zimasanya@gmail.com>
2022-08-04 14:04:15 +0300

    Update 01.download_DEM.sh

--- 01.download_DEM.sh
+++ 01.download_DEM.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+user="<user EarthExplorer>"
+password="<password EarthExplorer>"
+
 main_dir=$(pwd)
 
 . ${main_dir}/config
 . ${main_dir}/regions
 
-work_dir="${main_dir}/..data//dem_data/SRTMGL1/${country}/${country_hgt}"
+work_dir="${main_dir}/../data/dem_data/SRTMGL1/${country}/${country_hgt}"
 mkdir -p $work_dir
 
-server_url="http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11"
+server_url="https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11"
 #server_url="https://step.esa.int/auxdata/dem/SRTMGL1"
 
 file_suffix="SRTMGL1.hgt.zip"
@@ -23,14 +26,14 @@ for i in $(seq $hgt_min_lat $hgt_max_lat) ; do
 	    --mirror \
 	    --continue \
 	    --quiet \
-	    --user=<user EarthExplorer> --password=<password EarthExplorer> \
+	    --user=${user} --password=${password} \
 	    --output-document=${work_dir}/${file_name} \
 	    "${server_url}/${file_name}"
 
-	if [ -s ${work_dir}/$file_name ] ; then
+	if [ -s ${work_dir}/${file_name} ] ; then
 	    echo "done."
 	else
-	    rm -f ${work_dir}/$file_name
+	    rm -f ${work_dir}/${file_name}
 	    echo "not tile, cleared."
 	fi
     done
