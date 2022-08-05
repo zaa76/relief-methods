4a3bd9c7b210dfda228703829dba1c8b88b4b752
zaa76 <zimasanya@gmail.com>
2022-08-05 11:27:51 +0300

    Update and rename 02.country.unzip.sh to 02.country.extract.sh

--- other_dem/FABDEM/02.country.extract.sh
+++ other_dem/FABDEM/02.country.extract.sh
@@ -19,20 +19,12 @@ for i in $(seq $min_lat $max_lat) ; do
     for j in $(seq $min_lon $max_lon) ; do
 	file_name="${lat_semisphere}${i}${lon_semisphere}$(printf %03d ${j})_FABDEM_V1-0.tif"
 	for f in *.zip; do
-
-#echo "> Arc: $f"
 	    file_exist=`unzip -l $f | grep $file_name &>/dev/null && echo true || echo false`
 	    if [[ "${file_exist}" = "true" ]] ; then
-
-#echo " ---> $file_name"
-
 		echo -n "${file_name}: "
 		unzip $f $file_name -d $work_dir &>/dev/null
 		echo "done."
 		break
-#	    else
-#		echo "${file_name}: not exist in archive."
-#		break
 	    fi
 	done
     done
